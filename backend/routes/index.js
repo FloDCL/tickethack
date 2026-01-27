2248557263bdc14f798c1bf9303de2c0e42df998
Florian DCL <docruzeiro.f@gmail.com>
2026-01-27 10:20:06 +0000

    création des routes departure et arrival

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const trip = require("../models/trips");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/trajet", (req, res) => {
+  trip.find().then((data) => {
+    res.json({ trips: data });
+    //console.log(data);
+  });
+});
+
+router.get("/trajet/departure/:departure", (req, res) => {
+  trip
+    .find({
+      departure: { $regex: new RegExp(req.params.departure, "i") },
+    })
+    .then((cityFound) => {
+      if (cityFound) {
+        res.json({ result: true, trajet: cityFound });
+      } else {
+        res.json({ result: false, error: "City not found" });
+      }
+    });
+});
+
+router.get("/trajet/arrival/:arrival", (req, res) => {
+  trip
+    .find({
+      arrival: { $regex: new RegExp(req.params.arrival, "i") },
+    })
+    .then((cityFound) => {
+      if (cityFound) {
+        res.json({ result: true, trajet: cityFound });
+      } else {
+        res.json({ result: false, error: "City not found" });
+      }
+    });
 });
 
 module.exports = router;
