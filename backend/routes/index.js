eeb1dfaa5902ed746593199b97cfba87b876ce0b
Florian DCL <docruzeiro.f@gmail.com>
2026-01-27 10:59:20 +0000

    creation de la route post pour capter departure arrival et date

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const trip = require("../models/trips");
+const moment = require("moment");
 
 router.get("/trajet", (req, res) => {
   trip.find().then((data) => {
@@ -33,6 +34,38 @@ router.get("/trajet/arrival/:arrival", (req, res) => {
         res.json({ result: true, trajet: cityFound });
       } else {
         res.json({ result: false, error: "City not found" });
+      }
+    });
+});
+
+router.post("/search", (req, res) => {
+  const { departure, arrival, date } = req.body;
+
+  let searchDateStart = moment(date).startOf("day");
+  const searchDateEnd = moment(date).endOf("day");
+
+  if (moment(date).isSame(moment(), "day")) {
+    searchDateStart = moment();
+  }
+
+  if (!departure || !arrival || !date) {
+    return res.json({ result: false, error: "No trip found" });
+  }
+
+  trip
+    .find({
+      departure: { $regex: new RegExp(departure, "i") },
+      arrival: { $regex: new RegExp(arrival, "i") },
+      date: { $gte: searchDateStart, $lte: searchDateEnd },
+    })
+    .then((data) => {
+      if (data.length > 0) {
+        res.json({ result: true, trips: data });
+      } else {
+        res.json({
+          result: false,
+          error: "No trip found",
+        });
       }
     });
 });
